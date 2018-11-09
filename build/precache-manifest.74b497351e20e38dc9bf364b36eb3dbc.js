240f30570d46195152a596b7cbbbf2be2de0d2df
Daniel Torres <dtorres.juarez@gmail.com>
2018-11-09 10:26:21 -0600

    primer commit

---
+++ build/precache-manifest.74b497351e20e38dc9bf364b36eb3dbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a009bae01e0034975150",
+    "url": "/static/js/main.a009bae0.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "a009bae01e0034975150",
+    "url": "/static/css/main.1d893233.chunk.css"
+  },
+  {
+    "revision": "20b5edd0cf9c7936c19ccc92789e77b5",
+    "url": "/index.html"
+  }
+];
